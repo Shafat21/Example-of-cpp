44319d2781c1d9bf819d300cb7e0e704cda9f600
Shafat Alam <Shafat.mahtab@gmail.com>
2022-11-12 12:47:03 +0600

    example3

--- example3.cpp
+++ example3.cpp
@@ -8,4 +8,12 @@ int main()
     cout << "Enter Three Numbers";
     cin >> n1 >> n2 >> n3;
 
+    if(n1 >= n2 && n1 >= n3)
+        cout << "Largest number: " << n1;
+
+    if(n2 >= n1 && n2 >= n3)
+        cout << "Largest number: " << n2;
+    
+    if(n3 >= n1 && n3 >= n2)
+        cout << "Largest number: " << n3;
 }
