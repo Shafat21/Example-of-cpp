375e93f8f6a8b4559a6ef3deba0c7635a6e758e0
Shafat Alam <Shafat.mahtab@gmail.com>
2022-11-12 12:56:01 +0600

    C++ Program to Find All Roots of a Quadratic Equation
    C++ Program to Find All Roots of a Quadratic Equation

--- example4.cpp
+++ example4.cpp
@@ -9,6 +9,7 @@ int main() {
     cin >> a >> b >> c;
     discriminant = b*b - 4*a*c;
     
+    // discriminant > 0
     if (discriminant > 0) {
         x1 = (-b + sqrt(discriminant)) / (2*a);
         x2 = (-b - sqrt(discriminant)) / (2*a);
@@ -16,13 +17,14 @@ int main() {
         cout << "x1 = " << x1 << endl;
         cout << "x2 = " << x2 << endl;
     }
-    
+    // discriminant = 0
     else if (discriminant == 0) {
         cout << "Roots are real and same." << endl;
         x1 = -b/(2*a);
         cout << "x1 = x2 =" << x1 << endl;
     }
 
+    //discriminant < 0
     else {
         realPart = -b/(2*a);
         imaginaryPart =sqrt(-discriminant)/(2*a);
@@ -31,5 +33,4 @@ int main() {
         cout << "x2 = " << realPart << "-" << imaginaryPart << "i" << endl;
     }
 
-    return 0;
 }
