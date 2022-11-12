8223720db321d536c01d76098d901c0689163bac
Shafat Alam <Shafat.mahtab@gmail.com>
2022-11-12 13:00:55 +0600

    Update example6.cpp

--- example6.cpp
+++ example6.cpp
@@ -3,17 +3,16 @@ using namespace std;
 
 int main() {
 
-  int year;
+    int year;
 
-  cout << "Enter a year: ";
-  cin >> year;
+    cout << "Enter a year: ";
+    cin >> year;
 
-  if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
-    cout << year << " is a leap year.";
-  }
-  else {
-    cout << year << " is not a leap year.";
-  }
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+        cout << year << " is a leap year.";
+    }
+    else {
+        cout << year << " is not a leap year.";
+    }
 
-  return 0;
 }
